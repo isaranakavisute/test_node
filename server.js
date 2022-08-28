0d1313fce0c6d2c9262cd916c5fd41ba406701fe
Isara Nakavisute <isara_nakavisute@hotmail.com>
2022-08-28 07:55:42 +0700

    Change Version 2.0

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World of My Node.JS Version 1.0');
+  res.end('Hello World of My Node.JS Version 2.0');
 }).listen(8080);
 
 console.log("server starting at port 8080");
