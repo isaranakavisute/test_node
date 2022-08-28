fa481bb25e91c6168e056e50c3c547bc79cd2fee
Isara Nakavisute <isara_nakavisute@hotmail.com>
2022-08-28 07:51:00 +0700

    TEST_NODE

---
+++ server.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World of My Node.JS');
+}).listen(8080);
+
+console.log("server starting at port 8080");
