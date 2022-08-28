f30943fe37292948868630ceb23fbe62b1b2bf07
Isara Nakavisute <isara_nakavisute@hotmail.com>
2022-08-28 08:51:00 +0700

    Change to 10.0

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World of My Node.JS Version 9.0');
+  res.end('Hello World of My Node.JS Version 10.0');
 }).listen(8080);
 
 console.log("server starting at port 8080");
